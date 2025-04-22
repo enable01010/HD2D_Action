781deb230b981c5cfafcf18ed2f3015cdd7eea8e
trikutovgateacher <t.rikuto.vga.teacher@gmail.com>
2025-04-22 17:36:00 +0900

    白上をシーン上において動作テスト

---
+++ Assets/00_Taniyama/Image/Character/Fubuki/TopDownCharacterControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cainos.PixelArtTopDown_Basic
+{
+    public class TopDownCharacterControllerTest : MonoBehaviour
+    {
+        public float speed;
+
+
+        private void Update()
+        {
+            Vector2 dir = Vector2.zero;
+            if (Input.GetKey(KeyCode.A))
+            {
+                dir.x = -1;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                dir.x = 1;            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                dir.y = 1;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                dir.y = -1;
+            }
+
+            dir.Normalize();
+
+            GetComponent<Rigidbody2D>().velocity = speed * dir;
+        }
+
+        public void SpeedUp()
+        {
+            speed += 0.5f;
+        }
+
+        public void SpeedDown()
+        {
+            speed -= 0.5f;
+        }
+
+    }
+
+}
